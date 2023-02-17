30b64a53983982e3fdc82270b41f0790640603d6
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-02-17 15:13:34 -0300

    Create Find Animal by ID service

---
+++ server/src/services/Animals/FindAnimalById.ts
@@ -0,0 +1,18 @@
+import AppError from "../../error/AppError";
+import { AnimalsRepository } from "../../repositories/AnimalsRepository";
+
+export class FindAnimalById {
+  constructor (
+    private animalsRepository: AnimalsRepository,
+  ) {}
+
+  async execute(id: string) {
+    const animal = this.animalsRepository.findById(id);
+
+    if(!animal) {
+      throw new AppError("Animal does not exists.");
+    }
+
+    return animal;
+  }
+}
