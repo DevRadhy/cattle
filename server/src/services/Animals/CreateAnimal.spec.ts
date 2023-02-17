81671f2afe40572170807b9692a7c7881622a83e
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-02-17 08:34:57 -0300

    Animal tests

--- server/src/services/Animals/CreateAnimal.spec.ts
+++ server/src/services/Animals/CreateAnimal.spec.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from "vitest";
+import AppError from "../../error/AppError";
 import { InMemoryAnimalsRepository } from "../../tests/repositories/InMemoryAnimalsRepository";
 import { CreateAnimalService } from "./CreateAnimalService";
 
@@ -16,5 +17,43 @@ describe("Create Animal", () => {
     });
 
     expect(animal).toBeTruthy();
+  });
+
+  it("Should not be able to create a new animal without identification", () => {
+    const animalsRepository = new InMemoryAnimalsRepository();
+    const createAnimal = new CreateAnimalService(animalsRepository);
+
+    expect(() => {
+      return createAnimal.execute({
+        identification: "",
+        fatherId: "001",
+        motherId: "002",
+        birthDate: new Date(),
+        weight: 40,
+      });
+    }).rejects.toThrow(AppError);
+  });
+
+  it("Should not be able to create a animail with a identification that already exists", async () => {
+    const animalsRepository = new InMemoryAnimalsRepository();
+    const createAnimal = new CreateAnimalService(animalsRepository);
+
+    await createAnimal.execute({
+      identification: "003",
+      fatherId: "001",
+      motherId: "002",
+      birthDate: new Date(),
+      weight: 40,
+    });
+
+    expect(() => {
+      return createAnimal.execute({
+        identification: "003",
+        fatherId: "001",
+        motherId: "002",
+        birthDate: new Date(),
+        weight: 40,
+      });
+    }).rejects.toThrow(AppError);
   });
 });
