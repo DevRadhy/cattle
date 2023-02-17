7efc509670fb1682fe70ea5d62930bd257243606
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-02-17 15:45:11 -0300

    Add save and findByID methods

--- server/src/repositories/AnimalsRepository.ts
+++ server/src/repositories/AnimalsRepository.ts
@@ -2,4 +2,6 @@ import Animal from "../entities/Animal";
 
 export abstract class AnimalsRepository {
   abstract create(props: Animal): Promise<void>;
+  abstract findById(id: string): Promise<Animal | null>;
+  abstract save(animal: Animal): Promise<void>;
 }
