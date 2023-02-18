5eb21f3674f2cce297bb31421679e1a8e4893bba
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-02-17 22:13:35 -0300

    Add delete method

--- server/src/repositories/AnimalsRepository.ts
+++ server/src/repositories/AnimalsRepository.ts
@@ -4,4 +4,5 @@ export abstract class AnimalsRepository {
   abstract create(props: Animal): Promise<void>;
   abstract findById(id: string): Promise<Animal | null>;
   abstract save(animal: Animal): Promise<void>;
+  abstract delete(id: string): Promise<void>;
 }
