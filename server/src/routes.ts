8333485cab929a3ed5c88dfd8ab904a62464df7d
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-02-26 10:39:48 -0300

    Create animal create router

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { createAnimalController } from "./controllers/Animals";
 
 const routes = Router();
+
+routes.post("/animals", (request, response) => {
+  return createAnimalController.handle(request, response);
+});
 
 export { routes };
