1fb5480a4aa80594c0d692eea1bb316d149abb09
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-02-27 15:03:00 -0300

    Create user create test

---
+++ server/src/services/Users/CreateUser.spec.ts
@@ -0,0 +1,43 @@
+import { describe, it, expect } from "vitest";
+import AppError from "../../error/AppError";
+import { InMemoryUsersRepository } from "../../tests/repositories/InMemoryUsersRepository";
+import { CreateUsers } from "./CreateUser";
+
+describe("Create User", () => {
+  it("Should be able to create a new user", async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository();
+    const createUser = new CreateUsers(inMemoryUsersRepository);
+
+    const { user } = await createUser.execute({
+      email: "john@mail.com",
+      name: "John Doe",
+    });
+
+    expect(inMemoryUsersRepository.users.length).toBe(1);
+    expect(inMemoryUsersRepository.users[0]).toEqual(user);
+    expect(user).toEqual({
+      _id: user.id,
+      props: {
+        name: "John Doe",
+        email: "john@mail.com",
+      }
+    });
+  });
+
+  it("Should not be able to create a new user with a exiting email", async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository();
+    const createUser = new CreateUsers(inMemoryUsersRepository);
+
+    await createUser.execute({
+      email: "john@mail.com",
+      name: "John Doe",
+    });
+
+    expect(() => {
+      return createUser.execute({
+        email: "john@mail.com",
+        name: "John Doe",
+      });
+    }).rejects.toThrow(AppError);
+  });
+});
