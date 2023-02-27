0aa30dcea0c5df84de3c40413c73d7ab78bfae2b
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-02-27 07:26:14 -0300

    Create animals routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,27 @@
 import { Router } from "express";
-import { createAnimalController } from "./controllers/Animals";
+import {
+  createAnimalController,
+  deleteAnimalController,
+  findAnimalByIdController,
+  updateAnimalController
+} from "./controllers/Animals";
 
 const routes = Router();
 
 routes.post("/animals", (request, response) => {
   return createAnimalController.handle(request, response);
+});
+
+routes.get("/animals/:id", (request, response) => {
+  return findAnimalByIdController.handle(request, response);
+});
+
+routes.put("/animals/:id", (request, response) => {
+  return updateAnimalController.handle(request, response);
+});
+
+routes.delete("/animals/:id", (request, response) => {
+  return deleteAnimalController.handle(request, response);
 });
 
 export { routes };
