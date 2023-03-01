7dc4fd470f57a1465d5a1777f136bd9fe600078a
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-03-01 18:34:39 -0300

    Create delete user test

---
+++ server/src/services/Users/DeleteUser.spec.ts
@@ -0,0 +1,32 @@
+import { randomUUID } from "crypto";
+import { describe, it, expect } from "vitest";
+import AppError from "../../error/AppError";
+import { InMemoryUsersRepository } from "../../tests/repositories/InMemoryUsersRepository";
+import { CreateUser } from "./CreateUser";
+import { DeleteUser } from "./DeleteUser";
+
+describe("Delete user", () => {
+  it("Should be able to delete a user", async () => {
+    const inMemmoryUsersRepository = new InMemoryUsersRepository();
+    const createUser = new CreateUser(inMemmoryUsersRepository);
+    const deleteUser = new DeleteUser(inMemmoryUsersRepository);
+
+    const { user } = await createUser.execute({
+      name: "John Doe",
+      email: "john@mail.com",
+    });
+
+    await deleteUser.execute(user.id);
+
+    expect(inMemmoryUsersRepository.users.length).toBe(0);
+  });
+
+  it("Should not be able to delete a invalid user", async () => {
+    const inMemmoryUsersRepository = new InMemoryUsersRepository();
+    const deleteUser = new DeleteUser(inMemmoryUsersRepository);
+
+    expect(() => {
+      return deleteUser.execute(randomUUID());
+    }).rejects.toThrow(AppError);
+  });
+});
