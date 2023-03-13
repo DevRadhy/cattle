238f56ac74b279dda3d7821b5f66bc8deb1b7572
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-03-13 20:50:30 -0300

    Fix authenticate user typo

--- server/src/services/Users/index.ts
+++ server/src/services/Users/index.ts
@@ -1,5 +1,5 @@
 import { prismaUsersRepository } from "../../database";
-import { AthenticateUser } from "./AuthenticateUser";
+import { AuthenticateUser } from "./AuthenticateUser";
 import { CreateUser } from "./CreateUser";
 import { DeleteUser } from "./DeleteUser";
 import { FindUserByEmail } from "./FindUserByEmail";
@@ -12,7 +12,7 @@ const findUserById = new FindUserById(prismaUsersRepository);
 const updateUser = new UpdateUser(prismaUsersRepository);
 const deleteUser = new DeleteUser(prismaUsersRepository);
 
-const authenticateUser = new AthenticateUser(prismaUsersRepository);
+const authenticateUser = new AuthenticateUser(prismaUsersRepository);
 
 export {
   createUser,
