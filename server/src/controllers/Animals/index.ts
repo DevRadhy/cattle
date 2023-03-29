80de7f74b1a7baa45b71cfccd38dc2d164790838
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-03-29 08:56:27 -0300

    Export find many animals controller

--- server/src/controllers/Animals/index.ts
+++ server/src/controllers/Animals/index.ts
@@ -1,17 +1,20 @@
-import { createAnimal, findAnimalbyId, updateAnimal, deleteAnimal } from "../../services/Animals";
+import { createAnimal, findAnimalbyId, updateAnimal, deleteAnimal, findManyAnimals } from "../../services/Animals";
 import { CreateAnimalController } from "./CreateAnimalController";
 import { DeleteAnimalController } from "./DeleteAnimalController";
 import { FindAnimalByIdController } from "./FindAnimalByIdController";
+import { FindManyAnimalsController } from "./FindManyAnimalsController";
 import { UpdateAnimalController } from "./UpdateAnimalController";
 
 const createAnimalController = new CreateAnimalController(createAnimal);
 const findAnimalByIdController = new FindAnimalByIdController(findAnimalbyId);
 const updateAnimalController = new UpdateAnimalController(updateAnimal);
 const deleteAnimalController = new DeleteAnimalController(deleteAnimal);
+const findManyAnimalsController = new FindManyAnimalsController(findManyAnimals);
 
 export {
   createAnimalController,
   findAnimalByIdController,
   updateAnimalController,
-  deleteAnimalController
+  deleteAnimalController,
+  findManyAnimalsController,
 };
