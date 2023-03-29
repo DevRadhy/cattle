2bdee52217aabeae89f125f58755cb9785811f06
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-03-29 08:20:51 -0300

    Create users routes (#37)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,6 +5,15 @@ import {
   findAnimalByIdController,
   updateAnimalController
 } from "./controllers/Animals";
+import {
+  authenticateUserController,
+  createUserController,
+  deleteUserController,
+  findUserByEmailController,
+  findUserByIdController,
+  updateUserController
+} from "./controllers/Users";
+import { authorizate } from "./middlewares";
 
 const routes = Router();
 
@@ -22,6 +31,31 @@ routes.put("/animals/:id", (request, response) => {
 
 routes.delete("/animals/:id", (request, response) => {
   return deleteAnimalController.handle(request, response);
+});
+
+routes.post("/login", async (request, response) => {
+  return authenticateUserController.handle(request, response);
+});
+
+routes.post("/users/register", (request, response) => {
+  return createUserController.handle(request, response);
+});
+
+routes.get("/users/:id", (request, response) => {
+  return findUserByIdController.handle(request, response);
+});
+
+routes.get("/users/", (request, response) => {
+  return findUserByEmailController.handle(request, response);
+});
+
+
+routes.patch("/users/:id", authorizate.verify, (request, response) => {
+  return updateUserController.handle(request, response);
+});
+
+routes.delete("/users/:id", authorizate.verify, (request, response) => {
+  return deleteUserController.handle(request, response);
 });
 
 export { routes };
