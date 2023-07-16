6a22b2585c3f5b82f95d02b8bdb3535804862b4f
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-07-16 19:04:08 -0300

    Add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,9 +2,11 @@ import "express-async-errors";
 import express, { NextFunction, Request, Response } from "express";
 import AppError from "./error/AppError";
 import { routes } from "./routes";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
