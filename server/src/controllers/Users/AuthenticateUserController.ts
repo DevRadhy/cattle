0c09195b137381f14d26ce205575e972b01e5f87
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-07-20 13:21:47 -0300

    Return user information in response body (#53)

--- server/src/controllers/Users/AuthenticateUserController.ts
+++ server/src/controllers/Users/AuthenticateUserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { AuthenticateUser } from '../../services/Users/AuthenticateUser';
+import { UsersViewModel } from '../../views/UsersViewModel';
 
 export class AuthenticateUserController {
   constructor (
@@ -9,11 +10,11 @@ export class AuthenticateUserController {
   async handle(request: Request, response: Response) {
     const { email, password } = request.body;
 
-    const token = await this.authenticateUser.execute({
+    const { token , user } = await this.authenticateUser.execute({
       email,
       password,
     });
 
-    return response.status(200).json({ token });
+    return response.status(201).json({ token , ...UsersViewModel.toHTTP(user) });
   }
 }
