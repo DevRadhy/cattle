847b30fdfee323917780b7ae9355c2cfb2f0a891
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2024-02-22 22:48:22 -0300

    add zod

--- server/src/controllers/Animals/CreateAnimalController.ts
+++ server/src/controllers/Animals/CreateAnimalController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { AnimalsViewModel } from "../../views/AnimalsViewModel";
 import { CreateAnimal } from "../../services/Animals/CreateAnimal";
+import { z } from "zod";
 
 export class CreateAnimalController {
   constructor (
@@ -8,11 +9,21 @@ export class CreateAnimalController {
   ) {}
 
   async handle(request: Request, response: Response) {
-    const { identification, birthDate, fatherId, motherId, gender, weight, description } = request.body;
+    const CreateAnimalProps = z.object({
+      identification: z.string(),
+      birthDate: z.string(),
+      fatherId: z.string().optional(),
+      motherId: z.string().optional(),
+      gender: z.number(),
+      weight: z.number(),
+      description: z.string().optional(),
+    });
+
+    const { identification, birthDate, fatherId, motherId, gender, weight, description } = CreateAnimalProps.parse(request.body);
 
     const { animal } = await this.createAnimal.execute({
       identification,
-      birthDate,
+      birthDate: new Date(birthDate),
       fatherId,
       motherId,
       gender,
