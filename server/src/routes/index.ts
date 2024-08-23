edf9b594e7912d83a47f1e65963b961dc83ea07f
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2024-08-22 22:58:32 -0300

    feat: create production routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { animalsRoutes } from "./animalsRoutes";
 import { usersRoutes } from "./usersRoutes";
 import { authRoutes } from "./authRoutes";
+import { productionControlRoutes } from "./productionControlRoutes";
 
 const routes = Router();
 
 routes.use("/users", usersRoutes);
 routes.use("/animals", animalsRoutes);
 routes.use("/auth", authRoutes);
+routes.use("/production", productionControlRoutes);
 
 export { routes };
