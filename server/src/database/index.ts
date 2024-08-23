7deee21d892b330c8ad1841bc095dfe6ec42e908
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2024-08-22 22:57:46 -0300

    feat: export prisma production repository

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,9 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaAnimalsRepository } from "./prisma/PrismaAnimalsRepository";
 import { PrismaUsersRepository } from "./prisma/PrismaUsersRepository";
+import { PrismaProductionControlRepository } from "./prisma/PrismaProductionControlRepository";
 
 const prisma = new PrismaClient();
 const prismaAnimalsRepository = new PrismaAnimalsRepository(prisma);
 const prismaUsersRepository = new PrismaUsersRepository(prisma);
+const prismaProductionControl = new PrismaProductionControlRepository(prisma);
 
-export { prismaAnimalsRepository, prismaUsersRepository };
+export { 
+  prismaAnimalsRepository, 
+  prismaUsersRepository, 
+  prismaProductionControl 
+};
