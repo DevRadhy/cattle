7a52bc138388dcabaec40c030f9f66c4de2fc8a1
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2024-08-28 22:33:28 -0300

    refactor: change production props

--- server/src/dtos/AnimalsDTO.ts
+++
@@ -1,7 +0,0 @@
-export interface ProductionProps {
-  id: string;
-  animalId: string;
-  date: Date;
-  goal: string;
-  price: number;
-}
--- server/src/repositories/ProductionRepository.ts
+++ server/src/repositories/ProductionRepository.ts
@@ -1,9 +1,9 @@
-import { ProductionProps } from "../dtos/AnimalsDTO";
+import Production from "../entities/Production";
 
 export abstract class ProductionRepository {
-  abstract create(props: ProductionProps): Promise<void>;
-  abstract findByAnimalId(id: string): Promise<ProductionProps | null>;
-  abstract findMany(ownerId: string): Promise<ProductionProps[]>;
-  abstract save(props: ProductionProps): Promise<void>;
+  abstract create(props: Production): Promise<void>;
+  abstract findByAnimalId(id: string): Promise<Production | null>;
+  abstract findMany(ownerId: string): Promise<Production[]>;
+  abstract save(props: Production): Promise<void>;
   abstract delete(id: string): Promise<void>;
 }
