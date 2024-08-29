df5e79a8431b2ef8d1b8c96df7bcdaf5cada286e
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2024-08-28 22:43:46 -0300

    feat: create find many productions service

--- server/src/services/Production/index.ts
+++ server/src/services/Production/index.ts
@@ -1,8 +1,10 @@
 import { prismaProduction } from "../../database";
 import { CreateProduction } from "./createProduction";
+import { FindManyProductions } from "./findManyProductions";
 
 const productionRepository = prismaProduction;
 
 const createProduction = new CreateProduction(productionRepository);
+const findManyProductions = new FindManyProductions(productionRepository);
 
-export { createProduction };
+export { createProduction, findManyProductions };
